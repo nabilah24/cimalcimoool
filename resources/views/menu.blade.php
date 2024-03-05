852c738207ec1d6c415b43fea2dc5512633521d3
nabilah24 <nabilah.zakiyah.2205356@students.um.ac.id>
2024-03-06 06:16:09 +0700

    push menu

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,284 @@
+@extends('layout.master')
+
+@section('content')
+    {{-- hero start --}}
+    <div class="hero-menu">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-lg-5">
+                    <div class="intro-excerpt">
+                        <h1>Menu</h1>
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="hero-img-wrap">
+                        <img src="{{asset('assets/images/logo.png')}}" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- hero end --}}
+
+
+
+    <div class="untree_co-section product-section before-footer-section">
+        <div class="container">
+            <div class="row">
+
+                <!-- Start Column 1 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cimol-original.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cimol Original</h3>
+                        <strong class="product-price">10k</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 1 -->
+
+                <!-- Start Column 2 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cimol-keju.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cimol Keju</h3>
+                        <strong class="product-price">20K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 2 -->
+
+                <!-- Start Column 3 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cimol-puyuh.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cimol Puyuh</h3>
+                        <strong class="product-price">15K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 3 -->
+
+                <!-- Start Column 4 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cimol-keju.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cimol Rujak</h3>
+                        <strong class="product-price">12K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 4 -->
+
+
+                <!-- Start Column 1 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cibak.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cibak Original</h3>
+                        <strong class="product-price">10K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 1 -->
+
+                <!-- Start Column 2 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cibak.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cibak Bakso Original</h3>
+                        <strong class="product-price">10K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 2 -->
+
+                <!-- Start Column 3 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cibak.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cibak Bola Udang</h3>
+                        <strong class="product-price">12K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 3 -->
+
+                <!-- Start Column 4 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cibak.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cibak Chikuwa</h3>
+                        <strong class="product-price">12K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 4 -->
+
+                <!-- Start Column 1 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cibak.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cibak Dumpling Keju</h3>
+                        <strong class="product-price">14K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 1 -->
+
+                <!-- Start Column 2 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cimol-banjir.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cimol Banjir Kuah Original</h3>
+                        <strong class="product-price">12K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 2 -->
+
+                <!-- Start Column 3 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cimol-banjir.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cimol Banjir Kuah Telur</h3>
+                        <strong class="product-price">15K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 3 -->
+
+                <!-- Start Column 4 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/aci-gulung.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Aci Gulung</h3>
+                        <strong class="product-price">10K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 4 -->
+
+                <!-- Start Column 1 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/sate-aqci-telor.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Sate Aci Telor</h3>
+                        <strong class="product-price">10K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 1 -->
+
+                <!-- Start Column 2 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/cireng.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Cireng Rujak</h3>
+                        <strong class="product-price">10K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 2 -->
+
+                <!-- Start Column 3 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/sambel bojot.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Sambel Bojot</h3>
+                        <strong class="product-price">3K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 3 -->
+
+                <!-- Start Column 4 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/sate-aci-bojot.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Sate Aci Bojot</h3>
+                        <strong class="product-price">13K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 4 -->
+
+                <!-- Start Column 1 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/basreng bojot.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Basreng Bojot</h3>
+                        <strong class="product-price">13K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 1 -->
+
+                <!-- Start Column 2 -->
+                <div class="col-12 col-md-4 col-lg-3 mb-5">
+                    <a class="product-item" href="#">
+                        <img src="{{asset('assets/images/menu/aci-gulung-bojot.jpg')}}" class="img-fluid product-thumbnail">
+                        <h3 class="product-title">Aci Gulung Bojot</h3>
+                        <strong class="product-price">13K</strong>
+
+                        <span class="icon-cross">
+                            <img src="{{asset('assets/images/svg/cross.svg')}}" class="img-fluid">
+                        </span>
+                    </a>
+                </div>
+                <!-- End Column 2 -->
+
+            </div>
+        </div>
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('beranda');
 });
+
+Route::get('/menu', function () {
+    return view('menu');
+});
