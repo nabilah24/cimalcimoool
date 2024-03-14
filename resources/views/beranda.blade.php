662aa74762d102ab99b74613a6427787916bedf3
tanayafh-10 <tanayafadillah494@gmail.com>
2024-03-14 13:04:42 +0800

    update beranda

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -2,21 +2,6 @@
 
 @section('content')
 
-@if(session()->has('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{session('success')}}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-
-@endif
-
-@if(session()->has('LoginError'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{session('LoginError')}}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-
-@endif
 
     {{-- hero --}}
     <div class="-testimonial-xxl py-5 hero-header mb-5">
